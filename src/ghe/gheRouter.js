59b9265709eca71baaf67a77fb524a734084f50a
Eirik Årdal <ekstern_eirik.ardal@spv.no>
2017-12-06 15:38:02 +0100

    Fixed broken csproj

--- src/ghe/gheRouter.js
+++ src/ghe/gheRouter.js
@@ -2,7 +2,11 @@ import createRouter from '../infrastructure/routerFactory';
 
 const { router, execute } = createRouter();
 
-router.get('/', execute(async (req, res) => {
+router.post('/pull-request', execute(async (req, res) => {
+  const body = req.body;
+
+  
+
   res.json({ temp: 'orary' });
 }));
 
