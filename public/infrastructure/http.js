8b7d105273ed6b5fc40adfc46d2c7e3faa54bce4
Eirik Årdal <eaardal@outlook.com>
2017-12-07 23:09:52 +0100

    mwølkjfasø

--- public/infrastructure/http.js
+++ public/infrastructure/http.js
@@ -16,6 +16,35 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
+var errorStatus = function errorStatus(error) {
+  return error && error.statusCode ? error.statusCode : null;
+};
+
+var responseStatus = function responseStatus(res) {
+  return res && res.statusCode ? res.statusCode : null;
+};
+
+var getResponseStatus = function getResponseStatus(error, response) {
+  var eStatus = errorStatus(error);
+  var rStatus = responseStatus(response);
+
+  var status = void 0;
+
+  if (eStatus) {
+    status = eStatus;
+  } else if (rStatus) {
+    status = rStatus;
+  } else {
+    status = 502;
+  }
+
+  return status;
+};
+
+var stringifyIfExists = function stringifyIfExists(obj) {
+  return obj ? JSON.stringify(obj) : '';
+};
+
 var post = function () {
   var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(url, body) {
     return regeneratorRuntime.wrap(function _callee$(_context) {
@@ -37,14 +66,23 @@ var post = function () {
               _logger2.default.debug('Making POST request', options);
 
               (0, _request2.default)(options, function (error, response, responseBody) {
-                _logger2.default.debug('POST Response', { error: error, response: response, body: responseBody });
+                var status = getResponseStatus(error, response);
 
-                var status = response && response.statusCode ? response.statusCode : 502;
+                var errorString = stringifyIfExists(error);
+                var responseString = stringifyIfExists(response);
+                var bodyString = stringifyIfExists(responseBody);
+
+                _logger2.default.debug('POST Response', {
+                  status: status,
+                  error: errorString,
+                  res: responseString,
+                  body: bodyString
+                });
 
                 if (status >= 200 && status < 400) {
-                  resolve({ status: status, response: response });
+                  resolve({ status: status, res: responseString, body: bodyString });
                 } else {
-                  reject({ status: status, error: error, response: response });
+                  reject({ status: status, err: errorString, res: responseString, body: bodyString });
                 }
               });
             }));
