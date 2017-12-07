3ee6b0bd6beba4975093c55fd72f0c274c8342b4
Eirik Årdal <eaardal@outlook.com>
2017-12-07 23:08:15 +0100

    response handling

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # Octobiwan
 
+> Status: Prototype, do not use
+
 ## Use
+
+### Slack
+
+1. Create an `Incoming WebHook` integration
+1. Copy the `Webhook URL` value
+
+### GitHub
+
+1. In your GitHub repository, go to `Settings` -> `Webhooks` -> `Add webhook`
+1. `Payload URL`:
+   `https://octobiwan.herokuapp.com/github/hook?slackHook={Webhook URL}`
+1. `Content type`: `application/json`
+1. Select events to send to hook.
+1. Invoke events, see that they appear in Slack, as configured in your `Incoming
+   WebHook integration`.
+
+#### Supported events:
+
+TBA
 
 ## Development
 
--- src/infrastructure/http.js
+++ src/infrastructure/http.js
@@ -1,6 +1,29 @@
 import request from 'request';
 import log from './logger';
 
+const errorStatus = error => (error && error.statusCode ? error.statusCode : null);
+
+const responseStatus = res => (res && res.statusCode ? res.statusCode : null);
+
+const getResponseStatus = (error, response) => {
+  const eStatus = errorStatus(error);
+  const rStatus = responseStatus(response);
+
+  let status;
+
+  if (eStatus) {
+    status = eStatus;
+  } else if (rStatus) {
+    status = rStatus;
+  } else {
+    status = 502;
+  }
+
+  return status;
+};
+
+const stringifyIfExists = obj => (obj ? JSON.stringify(obj) : '');
+
 const post = async (url, body) =>
   new Promise((resolve, reject) => {
     const headers = {
@@ -17,14 +40,23 @@ const post = async (url, body) =>
     log.debug('Making POST request', options);
 
     request(options, (error, response, responseBody) => {
-      log.debug('POST Response', { error, response, body: responseBody });
+      const status = getResponseStatus(error, response);
 
-      const status = response && response.statusCode ? response.statusCode : 502;
+      const errorString = stringifyIfExists(error);
+      const responseString = stringifyIfExists(response);
+      const bodyString = stringifyIfExists(responseBody);
+
+      log.debug('POST Response', {
+        status,
+        error: errorString,
+        res: responseString,
+        body: bodyString,
+      });
 
       if (status >= 200 && status < 400) {
-        resolve({ status, response });
+        resolve({ status, res: responseString, body: bodyString });
       } else {
-        reject({ status, error, response });
+        reject({ status, err: errorString, res: responseString, body: bodyString });
       }
     });
   });
