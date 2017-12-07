9136ee749efbf608b83880cdb4934d4d6477ae6f
Eirik Årdal <eaardal@outlook.com>
2017-12-07 20:20:36 +0100

    Removed old project. Made cross-env non-dev dep

--- reference/GithubWebhookProcessor/GithubWebhookProcessor.Host/Controllers/Payload.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Spv.Tools.GithubWebhooks.Host.Controllers
-{
-    public class Payload
-    {
-        public string Text { get; set; }
-        public string Channel { get; set; }
-        public string Username => "GithubBot";
-    }
-}
